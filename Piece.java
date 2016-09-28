0678763d3015bd78dd2beace7e4eb1c3b0a08fd4
lukeclement <lclement.57468@farnborough.ac.uk>
2016-09-28 15:29:06 +0100

    Expanded player class

--- Piece.java
+++ Piece.java
@@ -2,85 +2,50 @@ class Piece{
   private int type;
   private int x;
   private int y;
+  private boolean alive=true;
 
   public Piece(int t,int x, int y){
     type=t;
     this.x=x;
     this.y=y;
   }
-
-  public int[] possibleX(){
-    switch(type){
-      case 0:
-      int[] moves=new int[8];
-      for(int i=0;i<moves.length;i++){
-        if(i==3){
-          int[i]=x;
-        }else if(i==4){
-          int[i]=x;
-        }else if(i-3<0){
-          int[i]=x-1;
-        }else{
-          int[i]=x+1
-        }
-      }
-      break;
-      case 1:
-      int[]
-      break;
-      case 2:
-      break;
-      case 3:
-      break;
-      case 4:
-      break;
-      case 5:
-      break;
-      case 6:
-      break;
-
-    }
-  }public int[] possibleY(){
-    switch(type){
-      case 0:
-      int[] moves=new int[8];
-      for(int i=0;i<moves.length;i++){
-        if(i>4){
-          switch(i%3){
-            case 0:int[i]=y+1;
-            break;
-            case 1:int[i]=y;
-            break;
-            case 2:int[i]=y-1;
-            break;
-          }
-        }else{
-          switch(i%3){
-            case 0:int[i]=y;
-            break;
-            case 1:int[i]=y-1;
-            break;
-            case 2:int[i]=y+1;
-            break;
-          }
-        }
-
-      }
-      break;
-      case 1:
-      break;
-      case 2:
-      break;
-      case 3:
-      break;
-      case 4:
-      break;
-      case 5:
-      break;
-      case 6:
-      break;
-
-    }
+  //Change in y+x
+  public void moving(int xc,int yc){
+    x=x+xc;
+    y=y+yc;
+    return;
   }
-
+  public void taking(){
+    alive=false;
+    return;
+  }
+  public void placing(int nx,int ny){
+    alive=true;
+    x=nx;
+    y=ny;
+    return;
+  }
+  //Getters
+  public int getY(){
+    return y;
+  }
+  public int getX(){
+    return x;
+  }
+  public int getType(){
+    return type;
+  }
+  //Setters
+  public void setY(int ny){
+    y=ny;
+    return;
+  }
+  public void setX(int nx){
+    x=nx;
+    return;
+  }
+  public void setType(int nt){
+    type=nt;
+    return;
+  }
 }
--- Player.java
+++ Player.java
@@ -1,3 +1,48 @@
 class Player{
-  
+  private int piecesLeft=16;
+  private int playerColor;
+  private Piece[] pieces=new Piece[piecesLeft];
+
+  public Player(int color){
+    playerColor=color;
+    if(playerColor==0){
+      for(int i=0;i<8;i++){
+        Piece[i]=new Piece(2,i+1,2);
+      }
+      Piece[8]=new Piece(0,5,1);
+      Piece[9]=new Piece(1,4,1);
+      Piece[10]=new Piece(3,1,1);
+      Piece[11]=new Piece(3,8,1);
+      Piece[12]=new Piece(4,2,1);
+      Piece[13]=new Piece(4,7,1);
+      Piece[14]=new Piece(5,3,1);
+      Piece[15]=new Piece(5,6,1);
+    }else{
+      for(int i=0;i<8;i++){
+        Piece[i]=new Piece(2,i+1,7);
+      }
+      Piece[8]=new Piece(0,5,8);
+      Piece[9]=new Piece(1,4,8);
+      Piece[10]=new Piece(3,1,8);
+      Piece[11]=new Piece(3,8,8);
+      Piece[12]=new Piece(4,2,8);
+      Piece[13]=new Piece(4,7,8);
+      Piece[14]=new Piece(5,3,8);
+      Piece[15]=new Piece(5,6,8);
+    }
+  }
+
+  public int[] getPieces(){
+    return pieces;
+  }
+  public void move(int p,int x,int y){
+    pieces[p].moving(x,y);
+  }
+  public void take(int p){
+    pieces[p].taking();
+  }
+  public void place(int p,int x,int y){
+    pieces[p].placing(x,y);
+  }
+
 }
